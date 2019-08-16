c7cdae5d68516dc83abcb6881dc0b2b80aa166ad
sdilts <stuart.dilts@gmail.com>
2019-08-15 22:31:10 -0600

    Refactor main.c and require -r flag for REPL
    + Only record items to history when the excecuted command had no
      parsing errors
    + Only save the history when exiting cleanly

--- salmon/main.cpp
+++ salmon/main.cpp
@@ -4,30 +4,63 @@
 #include <filesystem>
 #include <linenoise.h>
 
+#include <unistd.h>
+
 #include "util/environment.hpp"
 #include "compiler/parser.hpp"
 
-int main() {
+static void repl(const std::filesystem::path &config_dir) {
 	using namespace salmon;
-	std::filesystem::path config_dir = get_config_dir();
-	std::cout << "Using config dir: " << config_dir << std::endl;
-
-	std::filesystem::path history_file = config_dir / "history.txt";
+	const std::filesystem::path history_file = config_dir / "history.txt";
 
 	linenoiseHistoryLoad(history_file.string().c_str());
 
 	char *line;
 	while((line = linenoise(" > ")) != nullptr) {
 		if(line[0] != '\0') {
-			linenoiseHistoryAdd(line);
-			linenoiseHistorySave(history_file.string().c_str());
-			std::cout << "You entered " << line  << std::endl;
 
-			std::string token;
-			parser::ReadResult result = parser::read_from_string(line, token);
-			std::cout << "Result: " << result << "\n token: " << token << std::endl;
+			try {
+				std::string token;
+				parser::ReadResult result = parser::read_from_string(line, token);
+				std::cout << "Result: " << result << "\n token: " << token << std::endl;
+				linenoiseHistoryAdd(line);
+			} catch(const parser::ParseException &error) {
+				std::cout << error.build_error_str() << std::endl;
+			}
 		}
 		free(line);
 	}
+	linenoiseHistorySave(history_file.string().c_str());
+}
+
+int main(int argc ,char **argv) {
+	bool repl_flag = false;
+	bool invalid_flag = false;
+
+	char c;
+	while( (c = getopt(argc, argv, "r")) != -1) {
+		switch(c) {
+		case 'r':
+			repl_flag = true;
+			break;
+		case '?':
+			invalid_flag = true;
+			break;
+		default:
+			abort();
+		}
+	}
+
+	if (invalid_flag) {
+		return -1;
+	}
+
+	std::filesystem::path config_dir = salmon::get_config_dir();
+	std::cout << "Using config dir: " << config_dir << std::endl;
+
+	if (repl_flag) {
+		repl(config_dir);
+	}
+
 	return 0;
 }
