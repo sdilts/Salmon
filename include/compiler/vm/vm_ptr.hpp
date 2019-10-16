6ceba492647da218a81567e3165d6ecdc661be5b
sdilts <stuart.dilts@gmail.com>
2019-10-16 17:27:33 -0600

    Add untested vm_ptr

---
+++ include/compiler/vm/vm_ptr.hpp
@@ -0,0 +1,42 @@
+#ifndef SALMON_COMPILER_VM_MEMORY
+#define SALMON_COMPILER_VM_MEMORY
+
+#include <map>
+#include <compiler/box.hpp>
+
+namespace salmon::vm {
+
+	using namespace salmon::compiler;
+	class vm_ptr {
+
+	private:
+		static std::map<Box*,unsigned int> instances;
+
+		Box *ptr;
+		std::map<Box*,unsigned int>::iterator position;
+
+	public:
+		static std::vector<Box*> get_instances();
+
+		explicit vm_ptr(Box *);
+		vm_ptr(std::nullptr_t);
+		vm_ptr(const vm_ptr& other);
+		vm_ptr(vm_ptr&& moving) noexcept;
+		~vm_ptr();
+
+		Box& operator*() const;
+		Box* operator->() const;
+
+		explicit operator bool() const;
+
+		vm_ptr& operator=(const vm_ptr&);
+		vm_ptr& operator=(vm_ptr&&) noexcept;
+		vm_ptr& operator=(Box* newPtr);
+
+		Box* get() const;
+		void swap(vm_ptr& other) noexcept;
+	};
+	void swap(vm_ptr& first, vm_ptr& second);
+}
+
+#endif
