4485971660e7cfb0d705c88b6ef2b09f634ffe70
sdilts <stuart.dilts@gmail.com>
2019-10-16 18:50:49 -0600

    Add vm_ptr tests
    + Missing tests:
      - Assignment operator
      - move constructor and move assignment operator
      - ??

---
+++ test/compiler/vm/vm_ptr_tests.cpp
@@ -0,0 +1,114 @@
+#include <test/catch.hpp>
+
+#include <compiler/vm/vm_ptr.hpp>
+
+namespace salmon::vm {
+
+    SCENARIO( "A single vm_ptr records its root correctly.", "[vm_ptr]") {
+
+		GIVEN( "A vm_ptr with a non-null pointer to keep track of") {
+			Box *item = new Box();
+			vm_ptr ptr(item);
+
+			THEN( "The box and only the box is shown in vm_ptr::get_instances()") {
+				std::vector<Box*> instances = vm_ptr::get_instances();
+
+				REQUIRE(!instances.empty());
+				REQUIRE(instances.size() == 1);
+				REQUIRE(instances[0] == item);
+			}
+
+			WHEN( "The vm_ptr is deleted") {
+				ptr.~vm_ptr();
+
+				THEN( "The box is not in vm_ptr::get_instances()") {
+					std::vector<Box*> instances = vm_ptr::get_instances();
+					REQUIRE(instances.empty());
+				}
+			}
+		}
+	}
+
+	SCENARIO( "Two vm_ptrs work correctly when created with the same Box pointer", "[vm_ptr]") {
+		GIVEN( "Two vm_ptrs created with the same Box pointer") {
+			Box *item = new Box();
+			vm_ptr ptr1(item);
+			vm_ptr ptr2(item);
+
+			THEN( "The box and only the box is shown in vm_ptr::get_instances()") {
+				std::vector<Box*> instances = vm_ptr::get_instances();
+
+				REQUIRE(!instances.empty());
+				REQUIRE(instances.size() == 1);
+				REQUIRE(instances[0] == item);
+			}
+
+			WHEN("One is deleted") {
+				ptr2.~vm_ptr();
+
+				THEN( "The box is still in vm_ptr::get_instances") {
+					std::vector<Box*> instances = vm_ptr::get_instances();
+
+					REQUIRE(!instances.empty());
+					REQUIRE(instances.size() == 1);
+					REQUIRE(instances[0] == item);
+				}
+			}
+
+			WHEN("Both are deleted") {
+				ptr2.~vm_ptr();
+				ptr1.~vm_ptr();
+
+				THEN( "The vm_ptr::get_instances is empty") {
+					std::vector<Box*> instances = vm_ptr::get_instances();
+
+					REQUIRE(instances.empty());
+				}
+			}
+		}
+	}
+	SCENARIO( "Copying an vm_ptr should behave correctly", "[vm_ptr]") {
+		GIVEN("A vm_ptr managing a Box and a copy of the vm_ptr") {
+			Box *item = new Box();
+			vm_ptr ptr(item);
+			vm_ptr copy(ptr);
+
+			THEN("The copy points to the same object") {
+				REQUIRE(&*ptr == &*copy);
+			}
+			WHEN("The original is deleted") {
+				ptr.~vm_ptr();
+				THEN("The new one still works.") {
+					REQUIRE(&*copy == item);
+				}
+				THEN("The item is still in the instances list.") {
+					std::vector<Box*> instances = vm_ptr::get_instances();
+
+					REQUIRE(!instances.empty());
+					REQUIRE(instances[0] == item);
+				}
+			}
+
+			WHEN("Both are deleted") {
+				ptr.~vm_ptr();
+				copy.~vm_ptr();
+
+				THEN( "The vm_ptr::get_instances is empty") {
+					std::vector<Box*> instances = vm_ptr::get_instances();
+
+					REQUIRE(instances.empty());
+				}
+			}
+		}
+	}
+
+	SCENARIO("vm_ptrs initialized with nullptr still function") {
+		vm_ptr ptr(nullptr);
+
+		WHEN("The pointer is deleted") {
+			THEN("Nothing bad happens") {
+				ptr.~vm_ptr();
+			}
+		}
+	}
+}
