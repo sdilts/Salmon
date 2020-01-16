1acd22c18085de1db736743ebac2cc187d58d9d8
sdilts <stuart.dilts@gmail.com>
2020-01-16 16:37:12 -0700

    Init the basic types in the base vm package

--- salmon/compiler/vm/vm.cpp
+++ salmon/compiler/vm/vm.cpp
@@ -2,13 +2,29 @@
 
 namespace salmon::vm {
 
-	// static void init_types(VirtualMachine &vm) {
+	static void init_types(Package &base_package, TypeTable &t_table) {
+		vm_ptr<Symbol> str_symb = base_package.intern_symbol("const-string");
+		vm_ptr<Symbol> list_symb = base_package.intern_symbol("list");
+		vm_ptr<Symbol> int_symb = base_package.intern_symbol("int-32");
+		vm_ptr<Symbol> double_symb = base_package.intern_symbol("float-32");
 
-	// }
+		Type str =  { str_symb,  {}, "Constant string type used by the vm" };
+		Type list = { list_symb, {}, "Linked list used by the vm" };
+		Type int_type =    {int_symb, {}, "32 bit integer" };
+		Type double_type = {double_symb, {}, "32 bit floating point"};
+
+		t_table.insert(str);
+		t_table.insert(list);
+		t_table.insert(int_type);
+		t_table.insert(double_type);
+	}
 
 	VirtualMachine::VirtualMachine(const std::string &base_package) :
 		mem_manager{}, type_table{}, packages{} {
 		packages.emplace(std::string(base_package), Package(base_package, mem_manager));
+
+		auto base_pkg_iter = packages.find(base_package);
+	    init_types(base_pkg_iter->second, type_table);
 	}
 
 }
