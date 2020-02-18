27bd852489e1ce6096a7ee52876bc3c5573d3228
sdilts <stuart.dilts@gmail.com>
2020-02-18 11:32:08 -0700

    Add function.cpp to build

--- include/compiler/vm/function.hpp
+++ include/compiler/vm/function.hpp
@@ -10,37 +10,46 @@
 
 namespace salmon::vm {
 
-	using FuncType = std::function<Box(vm_ptr<List>)>;
-
 	class VmFunction {
 	public:
 		virtual ~VmFunction();
 
-		virtual Box operator()(vm_ptr<List> &args) const = 0;
+		const vm_ptr<Symbol> name;
+
+		/**
+		 * Call the underlying function with the given arguments
+		 *
+		 * Objects must be boxed
+		 **/
+		template<typename ... Types>
+		virtual Box operator()(Types ... args) = 0;
+
 		virtual void describe() const = 0;
 	private:
 		vm_ptr<List> lambda_list;
 
 		std::optional<std::string> documentation;
-		std::optional<std::filesystem::path> source_file;
+		std::optional<std::string> source_file;
 		std::optional<vm_ptr<List>> source_form;
 	};
 
 	class NormalFunction : public VmFunction {
 		~NormalFunction();
 	private:
-		FuncType function;
 		std::vector<Type*> arg_types;
 
 	public:
-	    Box operator()(vm_ptr<List> &args) const override;
+		template<typename ... Types>
+		Box operator()(Types ... args) override;
 		void describe() const override;
 	};
 
 	class InterfaceFunction : public VmFunction {
 	public:
 		~InterfaceFunction();
-		Box operator()(vm_ptr<List> &args) const override;
+
+		template<typename ... Types>
+		Box operator()(Types ... args) override;
 		void describe() const override;
 
 	private:
