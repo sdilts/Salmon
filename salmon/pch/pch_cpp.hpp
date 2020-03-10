1ed7620f908a28fffafb25d6582539c91960983d
sdilts <stuart.dilts@gmail.com>
2020-03-10 12:14:58 -0600

    Add precompiled header file for libsalmon

---
+++ salmon/pch/pch_cpp.hpp
@@ -0,0 +1,11 @@
+#include <exception>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <ostream>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
