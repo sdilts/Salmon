7dabb7eb84e92d14c024fb76faa13023c094ea97
sdilts <stuart.dilts@gmail.com>
2020-07-15 23:09:20 -0600

    Add vm_ptr comparison function in cmpunderlyingtype

--- include/util/cmpunderlyingtype.hpp
+++ include/util/cmpunderlyingtype.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <vm/vm_ptr.hpp>
+
 namespace salmon {
 	template<typename T>
 	struct cmpUnderlyingType {
 		bool operator()(const T* a,
 						const T* b) const {
+			return *a < *b;
+		}
+
+		bool operator()(const vm::vm_ptr<T> &a, const vm::vm_ptr<T> &b) const {
 			return *a < *b;
 		}
 	};
