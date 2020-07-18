f4e8e08224cab147fe614ec957acb4d49d02061b
sdilts <stuart.dilts@gmail.com>
2020-07-18 16:32:21 -0600

    Implement and register print functions for all types

--- salmon/vm/vm.cpp
+++ salmon/vm/vm.cpp
@@ -2,8 +2,68 @@
 
 #include <vm/vm.hpp>
 #include <vm/string.hpp>
+#include <vm/builtinfunction.hpp>
+#include "vmstdlib.cpp"
 
 namespace salmon::vm {
+
+	template<typename ... Args>
+	static void add_function(VirtualMachine *vm,
+							 const vm_ptr<Symbol> &name,
+							 typename BuiltinFunction<Args...>::FunctionType fn,
+							 const std::vector<vm_ptr<Symbol>> &lambda_list,
+							 const TypeSpecification &ret, const TypeSpecification &args) {
+		vm_ptr<Type> fn_type = vm->type_table.get_fn_type(ret, args);
+		vm_ptr<VmFunction> vm_fn(vm->mem_manager
+								 .allocate_obj<BuiltinFunction<Args...>>(fn,
+																   fn_type,
+																   lambda_list));
+		salmon_ensure(vm->fn_table.add_function(name, vm_fn), "Function not added");
+	}
+
+	static void init_print_fns(VirtualMachine *vm) {
+		FunctionTable &fn_table = vm->fn_table;
+		Package &base_package = vm->base_package();
+		TypeTable &type_table = vm->type_table;
+
+		vm_ptr<Symbol> obj_symb = base_package.intern_symbol("obj");
+		vm_ptr<Symbol> print_symb = base_package.intern_symbol("print");
+		SpecBuilder interfaceBuilder;
+		interfaceBuilder.add_parameter(obj_symb);
+		vm_ptr<Type> p_interface_type = type_table.get_fn_type(interfaceBuilder.build(),
+															   interfaceBuilder.build());
+		std::vector<vm_ptr<Symbol>> lambda_list = {obj_symb};
+		vm_ptr<InterfaceFunction> p_interface =
+			vm->mem_manager.allocate_obj<InterfaceFunction>(p_interface_type, lambda_list);
+		salmon_ensure(fn_table.new_interface(print_symb, p_interface),
+					  "Interface function not added");
+		base_package.export_symbol(print_symb);
+
+		std::vector<std::pair<vm_ptr<Type>,BuiltinFunction<InternalBox>::FunctionType>> to_add =
+		{
+			{ vm->get_builtin_type<List>(), print_list },
+			{ vm->get_builtin_type<Symbol>(),       print_pointer_primitive<Symbol> },
+			{ vm->get_builtin_type<StaticString>(), print_pointer_primitive<StaticString> },
+		    { vm->get_builtin_type<double>(),       print_primitive<double> },
+			{ vm->get_builtin_type<int32_t>(),      print_primitive<int32_t> },
+			{ vm->get_builtin_type<bool>(),         print_primitive<bool> },
+			{ vm->get_builtin_type<Empty>(),        print_primitive<Empty> },
+			{ vm->get_builtin_type<Array>(), print_array },
+		};
+
+		for(const auto &[type, fn] : to_add) {
+			SpecBuilder spec;
+			vm_ptr<Type> tmp_type = type;
+			spec.add_type(tmp_type);
+			add_function<InternalBox>(vm, print_symb, fn, lambda_list,
+									  spec.build(), spec.build());
+		}
+
+	}
+
+	static void init_stdlib(VirtualMachine *vm) {
+		init_print_fns(vm);
+	}
 
 	static void init_types(Package &base_package, TypeTable &t_table,
 						   std::unordered_map<std::type_index, Type*> &builtin_map) {
@@ -63,6 +123,7 @@ namespace salmon::vm {
 
 		Package &base_pkg = packages.find(base_package_name)->second;
 		init_types(base_pkg, type_table, builtin_map);
+		init_stdlib(this);
 	}
 
 	Package &VirtualMachine::base_package() {
---
+++ salmon/vm/vmstdlib.cpp
@@ -0,0 +1,63 @@
+#include <vm/vm.hpp>
+#include <iostream>
+
+namespace salmon::vm {
+
+	Box print_list(VirtualMachine *vm, InternalBox list) {
+		salmon_check(*list.type == *vm->get_builtin_type<List>(), "Given type is not a list");
+		List *first = std::get<List*>(list.elem);
+		vm_ptr<Symbol> print_symb = vm->base_package().intern_symbol("print");
+		vm_ptr<VmFunction> print_fn = *vm->fn_table.get_fn(print_symb);
+
+		std::vector<InternalBox> arg_vec = {first->itm };
+
+		std::cout << '(';
+		(*print_fn)(vm, arg_vec);
+		for(List *head = first->next; head != nullptr; head = head->next) {
+			std::cout << ' ';
+			arg_vec[0] = head->itm;
+			(*print_fn)(vm, arg_vec);
+		}
+		std::cout << ')';
+		Box ret(list, vm->mem_manager.make_vm_ptr<AllocatedItem>());
+		return ret;
+	}
+
+	Box print_array(VirtualMachine *vm, InternalBox box) {
+		salmon_check(*box.type == *vm->get_builtin_type<Array>(), "Given type is not an array");
+		vm_ptr<Symbol> print_symb = vm->base_package().intern_symbol("print");
+		vm_ptr<VmFunction> print_fn = *vm->fn_table.get_fn(print_symb);
+		Array *arr = std::get<Array*>(box.elem);
+
+		std::cout << '[';
+		auto iter = arr->begin();
+		if(iter != arr->end()) {
+			std::vector<InternalBox> arg_vec = { *iter };
+			(*print_fn)(vm, arg_vec);
+			for(iter = std::next(iter); iter != arr->end(); ++iter) {
+				std::cout << ' ';
+				arg_vec[0] = *iter;
+				(*print_fn)(vm, arg_vec);
+			}
+		}
+		std::cout << ']';
+		Box ret(box, vm->mem_manager.make_vm_ptr<AllocatedItem>());
+		return ret;
+	}
+
+	template<typename T>
+	Box print_pointer_primitive(VirtualMachine *vm, InternalBox box) {
+		salmon_check(*box.type == *vm->get_builtin_type<T>(), "Given type is not correct");
+		std::cout << *std::get<T*>(box.elem);
+		Box ret(box, vm->mem_manager.make_vm_ptr<AllocatedItem>());
+		return ret;
+	}
+
+	template<typename T>
+	Box print_primitive(VirtualMachine *vm, InternalBox box) {
+		salmon_check(*box.type == *vm->get_builtin_type<T>(), "Given type is not a correct");
+		std::cout << std::get<T>(box.elem);
+		Box ret(box, vm->mem_manager.make_vm_ptr<AllocatedItem>());
+		return ret;
+	}
+}
